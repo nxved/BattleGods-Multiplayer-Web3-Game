031acde9939e33ac3dc2f0e16958062bf043176f
Naved Khan <94339342+nxved@users.noreply.github.com>
2023-02-24 14:35:27 +0530

    added PageHOC

---
+++ client/src/components/PageHOC.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+import {logo, heroImg} from '../assets'
+import styles from '../styles';
+
+const PageHOC = (Component, title, description) =>() => {
+  const navigate = useNavigate();
+  return (
+    <div className={styles.hocContainer}>
+      <div className={styles.hocContentBox}>
+        <img src={logo} alt='logo' className={styles.hocLogo}
+        onClick={()=>navigate('/')} />
+        <div className={styles.hocBodyWrapper}>
+          <div>
+            <h1>{title}
+            </h1>
+          </div>
+          <p>{description}</p>
+        <Component/>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default PageHOC
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { PageHOC } from '../components';
 
 const Home = () => {
   return (
     <div>
-      <h1 className="text-5xl p-3">Avax Gods</h1>
-      <h2 className="text-3xl p-3">Web3 NFT Battle-style Card Game</h2>
-      <p className="text-xl p-3">Made with 💜 by JavaScript Mastery</p>
+      <h1 className="text-5xl p-3">Battle Gods</h1>
+      <h2 className="text-3xl p-3">NFT Battle Card Game</h2>
+      <p className="text-xl p-3">Made with</p>
     </div>
   )
 };
 
-export default Home;
+export default PageHOC(
+  Home, <>Welcome to Battle Gods <br/> a NFT card Game</>,
+  <>Conntect Your Wallet <br />The Ultimate Battle Card Game</>
+
+);
