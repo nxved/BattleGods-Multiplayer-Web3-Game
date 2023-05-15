012d13466460a3c39b223d7aa897c35105844082
Naved Khan <94339342+nxved@users.noreply.github.com>
2023-05-15 23:12:05 +0530

    context

--- client/src/context/createEventListeners.js
+++ client/src/context/createEventListeners.js
@@ -3,3 +3,12 @@ import { ABI } from "../contract";
 import { playAudio , sparcle} from "../utils/animation";
 import { defenseSound } from "../assets";
 
+const AddNewEvent = (eventFilter, provider, cb) =>{
+   provider.removeListener(eventFilter);
+
+   provider.on(eventFilter, (logs) => {
+      const parsedLogs = (new ethers.utils.Interface(ABI)).parseLog(logs);
+
+      cb(parsedLogs);
+   });
+}
