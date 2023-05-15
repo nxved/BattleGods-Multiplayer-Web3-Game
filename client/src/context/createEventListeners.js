6f935d2d07bf3698fb9ce014ac6f5f5a3fb8361c
Naved Khan <94339342+nxved@users.noreply.github.com>
2023-05-15 23:05:42 +0530

    context

---
+++ client/src/context/createEventListeners.js
@@ -0,0 +1,5 @@
+import { ethers } from "ethers";
+import { ABI } from "../contract";
+import { playAudio , sparcle} from "../utils/animation";
+import { defenseSound } from "../assets";
+
--- client/src/context/newIndex.js
+++
@@ -1,3 +0,0 @@
-import { useCallback } from "react";
-
-useCallback
